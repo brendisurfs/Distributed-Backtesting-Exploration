73ba5cc63a280e5ef2dfa5ccdbc814458a63c7b7
Brendan P. <64713032+brendisurfs@users.noreply.github.com>
2026-02-25 10:52:47 -0800

    fix: improved description of next tasks
    Signed-off-by: Brendan P. <64713032+brendisurfs@users.noreply.github.com>

--- src/worker/process.rs
+++ src/worker/process.rs
@@ -4,6 +4,12 @@ use flume::Sender;
 
 use crate::{backtesting::JobsReply, ms, PROC_FLAG};
 
+/// # process_incoming_job
+///
+/// Handles incoming job and does work.
+/// Note: currently only simulates doing heavy compute work.
+/// * `jobs_reply` - the response we get from the server of jobs to work on.
+/// * `complete_send` - channel to send completed work over.
 pub fn process_incoming_job(jobs_reply: JobsReply, complete_send: Sender<String>) {
     let Some(proc_flag) = PROC_FLAG.get() else {
         tracing::error!("No PROC_FLAG value set");
@@ -17,5 +23,7 @@ pub fn process_incoming_job(jobs_reply: JobsReply, complete_send: Sender<String>
         thread::sleep(ms(1000));
         let _ = complete_send.send(job.id.clone());
     }
+
+    // set our processing flag to false to signify we are available for work.
     proc_flag.store(false, Ordering::SeqCst);
 }
